284852209e3f9b8d027442896950209e24db2d0d
aryaman2803 <aryamansingh2803@gmail.com>
2021-03-09 22:57:35 +0530

    master

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Loading from "./isLoading";
+import { IoSearch } from "react-icons/io5";
+// import fetchData from "./fetchData";
+const url =
+  "https://emoji-api.com/emojis?access_key=0b123a2521de694967e202c8f1611a5419b3837d";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const [value, setValue] = useState("");
+  const [list, setList] = useState([]);
+
+  const fetchData = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(value);
+  };
+  if (isLoading) {
+    return (
+      <main> 
+        <Loading />
+      </main>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="section-search">
+        <form onSubmit={handleSubmit}>
+          <div>
+            <IoSearch />
+            <input
+              type="text"
+              value={value}
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <button type="submit">Search</button>
+          </div>
+        </form>
+      </section>
+    </main>
   );
 }
 
---
+++ src/isLoading.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+const Loading = () => {
+  return (
+    <main>
+      <div className="isloading">
+        <h1>loading...</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Loading;
